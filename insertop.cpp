bfd24c9c53052962e1db18026923fcc92f6cfbab
DanLangas <dan.langas@gmail.com>
2020-11-04 12:25:19 +0200

    added template and others

---
+++ insertop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		int x{ 5} ;
+		cout << "Hello" << " numarul:    " << x;
+		return 0;
+}
+
---
+++ template.cpp
@@ -0,0 +1,49 @@
+// template program in C++
+#include <iostream>
+using namespace std;
+
+int x; // define a variable named x, of type int
+double width; // define a variable named width, of type double
+
+
+int a;
+int b;
+/* is the same as: */
+
+int a, b;
+
+
+int width; // define an integer variable named width
+width = 5; // copy assignment of value 5 into variable width
+
+// variable width now has value 5
+//
+
+
+int width = 5; // copy initialization of value 5 into variable width
+
+
+int width( 5 ); // direct initialization of value 5 into variable width
+
+
+int main ()
+{
+
+/* multiline comment
+ * here
+ * and here */
+
+  cin >> x;
+  cout << "x are valoarea ", x;
+
+
+/* multiline comment
+ * here
+ * and here */
+
+
+
+
+/* nu uita sa salvezi in alt fisier */
+
+}
